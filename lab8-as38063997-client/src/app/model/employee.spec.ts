e4a93c3baf073e37883c6b70ef4e312e777108d4
Kuan-Yu Chang <grouper.chang@gmail.com>
2024-11-10 17:32:53 -0500

    fix

--- lab8-as38063997-client/src/app/model/employee.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Employee } from './employee';
-
-describe('Employee', () => {
-  it('should create an instance', () => {
-    expect(new Employee("1", "andrew", new Date().toISOString(), "London", 1000)).toBeTruthy();
-  });
-});
--- lab8-as38063997-server/src/main/kotlin/seg3x02/employeeGql/repository/EmployeeRepository.kt
+++
@@ -1,8 +0,0 @@
-package SEG3102.employeeGql.repository
-
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-import SEG3102.employeeGql.entity.Employee
-
-@Repository
-interface EmployeeRepository: MongoRepository<Employee, String>
