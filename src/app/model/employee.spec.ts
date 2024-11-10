1c758f636653fb20e9743d5bd63d4106628b3e5c
Kuan-Yu Chang <grouper.chang@gmail.com>
2024-11-10 16:47:23 -0500

    inital commit

--- src/app/model/employee.spec.ts
+++ src/app/model/employee.spec.ts
@@ -2,6 +2,6 @@ import { Employee } from './employee';
 
 describe('Employee', () => {
   it('should create an instance', () => {
-    expect(new Employee("andrew", new Date(), "London", 1000)).toBeTruthy();
+    expect(new Employee("1", "andrew", new Date().toISOString(), "London", 1000)).toBeTruthy();
   });
 });
